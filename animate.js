303755ab8d5fe5fec6fc8a0c16a3cf4155cae82d
Marc Doerflinger <marc@doerf.li>
2015-02-18 23:11:02 +0100

    format code

--- animate.js
+++ animate.js
@@ -1,63 +1,75 @@
+var shadesOfBlue = [ "signblue" , "bluespider", "alaskasky", "bluevelvet", "delft", "midnightblue", "royalblue4"];
+var width = window.innerWidth;
+var height = window.innerHeight;
+var blockwidth = 40; // px
+var blockheight = 30; // px
+// grid size
+var gridwidth = Math.round(width / blockwidth) - 1; // no clipped block at right end
+var gridheight = Math.round(height / blockheight);
+console.log( "grid size - x: " + gridwidth + " y: " + gridheight);
+// stacks to place the blocks on
+var stacks = new Array( gridwidth);
+
+// the number of the current element
+var numEl = 0;
+
+// move a box onto a stack
+var float = function(){
+  var box = createBox();
+  box.appendTo("body");
+
+  // calculate the target grid position of the block
+  var x = Math.floor((Math.random() * gridwidth));
+  // calculate the vertical position on the stack
+  var y = stacks[x]++;
+
+  // calculate the absolute screen position to move the block to
+  console.log( "goto x: " + x + " y: " + y);
+  var posx = x * blockwidth;
+  var posy = height - (y * blockheight) - blockheight; // stop above screen bottom
+
+  // now move the block
+  box.animate(
+    {left: posx+"px", top: posy+"px"},
+    { duration: 3000 }
+  );
+}
+
+var createBox = function() {
+  var content = createContent();
+  var box = $('.origbox').clone();
+  var startgridx = Math.floor((Math.random() * gridwidth));
+  var startx = startgridx * blockwidth;
+  var color = shadesOfBlue[Math.floor( Math.random() * shadesOfBlue.length)];
+
+  box.attr('id','box_' + numEl++);
+  box.removeClass( 'origbox');
+  box.css( 'visibility', 'visible');
+  box.css( 'left', startx + "px");
+  box.css( 'top', -blockheight + "px");
+  console.log( color);
+  var txt = box.find(".txt");
+  txt.text( content);
+  txt.addClass( color);
+  return box;
+}
+
+var createContent = function() {
+  var byte = Math.floor(Math.random() * 256);
+  var content = byte.toString(16).toUpperCase();
+  if ( content.length == 1 ) content = "0" + content;
+  return content;
+}
 
 
 $(document).ready(function() {
-  console.log("ready!");
-  var w = window.innerWidth;
-  var h = window.innerHeight;
-  var blockwidth = 40;
-  var blockheight = 30;
-  var gridmaxx = Math.round(w / blockwidth) - 1;
-  var gridmaxy = Math.round(h / blockheight);
-  console.log( "grid size - x: " + gridmaxx + " y: " + gridmaxy);
-  var stacks = new Array( gridmaxx);
-  var blues = [ "signblue" , "bluespider", "alaskasky", "bluevelvet", "delft", "midnightblue", "royalblue4"];
 
+  // initialize stack
   for ( var i = 0; i < stacks.length; i++) {
     stacks[i] = 0;
   }
 
-  var e = 0;
-
-  var float = function(){
-    var byte = Math.floor(Math.random() * 256);
-    var content = byte.toString(16).toUpperCase();
-    if ( content.length == 1 ) content = "0" + content;
-    var box = $('.origbox').clone();
-    var startgridx = Math.floor((Math.random() * gridmaxx));
-    var startx = startgridx * blockwidth;
-    var color = blues[Math.floor( Math.random() * blues.length)];
-
-    box.attr('id','box_' + e);
-    box.removeClass( 'origbox');
-    box.css( 'visibility', 'visible');
-    box.css( 'left', startx + "px");
-    box.css( 'top', -blockheight + "px");
-    console.log( color);
-    var txt = box.find(".txt");
-    txt.text( content);
-    txt.addClass( color);
-    box.appendTo("body");
-
-    console.log(box);
-    var x = Math.floor((Math.random() * gridmaxx));
-    var y = stacks[x];
-
-    console.log( "goto x: " + x + " y: " + y);
-    var posx = x * blockwidth;
-    var posy = h - (y * blockheight) - blockheight; // stop above screen bottom
-    stacks[x] = stacks[x] + 1;
-
-    box.animate(
-      {left: posx+"px", top: posy+"px"},
-      { duration: 3000,
-//        done: function()
-//        {
-//          float();
-//        }
-      }
-    );
-    e = e + 1;
-  }
+  console.log("May the floating begin!");
 
   float();
   setInterval( float, 600);
