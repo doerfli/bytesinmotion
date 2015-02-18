caad8d75a2eb9dca904cb4a51c44a6c51b06046d
Marc Doerflinger <marc@doerf.li>
2015-02-18 23:59:34 +0100

    make font-size relative to view width (30 columns)

--- animate.js
+++ animate.js
@@ -1,76 +1,80 @@
+// class names for blue text
 var shadesOfBlue = [ "signblue" , "bluespider", "alaskasky", "bluevelvet", "delft", "midnightblue", "royalblue4"];
-var width = window.innerWidth;
-var height = window.innerHeight;
-var blockwidth = 40; // px
-var blockheight = 30; // px
-// grid size
-var gridwidth = Math.round(width / blockwidth) - 1; // no clipped block at right end
-var gridheight = Math.round(height / blockheight);
-console.log( "grid size - x: " + gridwidth + " y: " + gridheight);
-// stacks to place the blocks on
-var stacks = new Array( gridwidth);
-
 // the number of the current element
 var numEl = 0;
 
-// move a box onto a stack
-var float = function(){
-  var box = createBox();
-  box.appendTo("body");
-
-  // calculate the target grid position of the block
-  var x = Math.floor((Math.random() * gridwidth));
-  // calculate the vertical position on the stack
-  var y = stacks[x]++;
-
-  // calculate the absolute screen position to move the block to
-  console.log( "goto x: " + x + " y: " + y);
-  var posx = x * blockwidth;
-  var posy = height - (y * blockheight) - blockheight; // stop above screen bottom
-
-  // now move the block
-  box.animate(
-    {left: posx+"px", top: posy+"px"},
-    { duration: 3000 }
-  );
-}
-
-var createBox = function() {
-  var content = createContent();
-  var box = $('.origbox').clone();
-  var startgridx = Math.floor((Math.random() * gridwidth));
-  var startx = startgridx * blockwidth;
-  var color = shadesOfBlue[Math.floor( Math.random() * shadesOfBlue.length)];
-
-  box.attr('id','box_' + numEl++);
-  box.removeClass( 'origbox');
-  box.css( 'visibility', 'visible');
-  box.css( 'left', startx + "px");
-  box.css( 'top', -blockheight + "px");
-  console.log( color);
-  var txt = box.find(".txt");
-  txt.text( content);
-  txt.addClass( color);
-  return box;
-}
-
-var createContent = function() {
-  var byte = Math.floor(Math.random() * 256);
-  var content = byte.toString(16).toUpperCase();
-  if ( content.length == 1 ) content = "0" + content;
-  return content;
-}
-
-
 $(document).ready(function() {
+  
+  var width = window.innerWidth;
+  var height = window.innerHeight;
+
+  // width => 30 stacks
+  // aspect ratio ~ 4:3
+  var blockwidth = Math.floor( width / 30); // px 
+  var blockheight = Math.floor( blockwidth / 4 * 3); // px 
+  console.log( "blockwidth: " + blockwidth + " x " + blockheight);
+
+  // grid size
+  var gridwidth = Math.round(width / blockwidth) - 1; // no clipped block at right end
+  var gridheight = Math.round(height / blockheight);
+  console.log( "grid size - x: " + gridwidth + " y: " + gridheight);
+  // stacks to place the blocks on
+  var stacks = new Array( gridwidth);
+
 
   // initialize stack
   for ( var i = 0; i < stacks.length; i++) {
     stacks[i] = 0;
   }
+  
+  // move a box onto a stack
+  var float = function(){
+    var box = createBox();
+    box.appendTo("body");
 
+    // calculate the target grid position of the block
+    var x = Math.floor((Math.random() * gridwidth));
+    // calculate the vertical position on the stack
+    var y = stacks[x]++;
+
+    // calculate the absolute screen position to move the block to
+    console.log( "float to gridx: " + x + " gridy: " + y);
+    var posx = x * blockwidth;
+    var posy = height - ((y + 1) * blockheight) - 10; // stop just above screen bottom
+
+    // now move the block
+    box.animate(
+      {left: posx+"px", top: posy+"px"},
+      { duration: 3000 }
+    );
+  }
+
+  var createBox = function() {
+    var content = createContent();
+    var box = $('.origbox').clone();
+    var startgridx = Math.floor((Math.random() * gridwidth));
+    var startx = startgridx * blockwidth;
+    var color = shadesOfBlue[Math.floor( Math.random() * shadesOfBlue.length)];
+
+    box.removeClass( 'origbox').attr('id','box_' + numEl++);
+    box.css( 'visibility', 'visible');
+    box.css( 'top', -blockheight + "px").css( 'left', startx + "px");
+    box.css( 'font-size', blockheight);
+    // insert text and set class
+    box.find(".txt").text( content).addClass( color);
+    return box;
+  }
+
+  var createContent = function() {
+    var byte = Math.floor(Math.random() * 256);
+    var content = byte.toString(16).toUpperCase();
+    if ( content.length == 1 ) content = "0" + content;
+    return content;
+  }
+  
   console.log("May the floating begin!");
 
   float();
   setInterval( float, 600);
+
 });
